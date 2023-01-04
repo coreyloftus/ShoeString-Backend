a080bd828f6e4fedfdbf8fad2a5085f8d45d55f1
coreyloftus <coreyloftus@gmail.com>
2023-01-04 09:59:06 -0500

    Posts Controller is fully CRUD

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -7,29 +7,53 @@ router.use(express.json())
 // http://localhost:4000/posts
 router.get("/", async (req, res, next) => {
     try {
-        res.status(200).json({message: "posts index route here"})
-    } catch(err) {
-        res.status(400).json({error:err})
+        res.status(200).json({ message: "posts index route here" })
+    } catch (err) {
+        res.status(400).json({ error: err })
         return next(err)
     }
 })
 
 // make it CRUD
 // CREATE
-router.post('/', async (req,res,next) => {
+router.post("/", async (req, res, next) => {
     try {
         const createPost = await Posts.create(req.body)
-        res.status(201).json({mesage: "created post", createPost})
-    } catch(err) {
-        res.status(400).json({error:err})
+        res.status(201).json({ mesage: "created post", createPost })
+    } catch (err) {
+        res.status(400).json({ error: err })
         return next(err)
     }
 })
 // READ
-
+router.get("/:id", async (req, res, next) => {
+    try {
+        const foundPost = await Posts.findById(req.params.id)
+        res.status(200).json({ foundPost })
+    } catch (err) {
+        res.status(400).json({ error: err })
+        return next(err)
+    }
+})
 // UPDATE
-
+router.put("/:id", async (req, res, next) => {
+    try {
+        const updatePost = await Posts.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        res.status(200).json({ message: "successfully updated", updatePost })
+    } catch (err) {
+        res.status(400).json({ error: err })
+        return next(err)
+    }
+})
 // DESTROY
-
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const deletedPost = await Posts.findByIdAndDelete(req.params.id)
+        res.status(200).json({ message: "successfully deleted", deletedPost })
+    } catch (err) {
+        res.status(400).json({ error: err })
+        return next(err)
+    }
+})
 
 module.exports = router
