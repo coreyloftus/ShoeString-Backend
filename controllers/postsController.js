91e929c8719eecd9fd88fedeef8c80b4f14a4e31
coreyloftus <coreyloftus@gmail.com>
2023-01-08 21:32:13 -0500

    updated Posts PUT route to allow for tags to be edited and properly created/assigned | added comment to CREATE route on how to add token to request header for auth

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -20,6 +20,7 @@ router.get("/", async (req, res, next) => {
 
 // make it CRUD
 // CREATE
+// token must be in the POST request header as {Authorization: Bearer (token here with no parenthesis or quotes)}
 router.post("/", requireToken, async (req, res, next) => {
     if (req.body.tags) {
         try {
@@ -63,6 +64,16 @@ router.get("/:id", async (req, res, next) => {
 router.put("/:id", requireToken, async (req, res, next) => {
     try {
         handleValidateOwnership(req, await Posts.findByIdAndUpdate(req.params.id))
+        // if user post includes tags, search to see if they exist
+        let foundTag = await Tags.findOne({ title: req.body.tags })
+        // if tag does NOT YET exist, create it and assign to req.body.tags
+        if (foundTag === null) {
+            const createTag = await Tags.create({ title: req.body.tags })
+            foundTag = createTag
+        }
+        // if tag DOES already exist, grab existing tag's ID and assign it to req.body.tags
+        // so that way all posts that use this tag reference the same tag
+        req.body.tags = foundTag._id
         const updatePost = await Posts.findByIdAndUpdate(req.params.id, req.body, { new: true })
         res.status(201).json({ message: "successfully updated", updatePost })
     } catch (err) {
